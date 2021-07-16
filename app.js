3c9fb686f7093e6a720ca679d13a869bd8057f3e
kachette <metelkinigor@gmail.com>
2021-07-16 19:59:04 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// Book Class: Represents a Book
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+
+// UI Class: Handle Ui Tasks
+
+
+
+//Store Class: Handles Storage 
+
+
+
+// Event: Display Books
+
+
+
+// Event: Remove A Book
